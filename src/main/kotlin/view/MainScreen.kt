d344afc9a6790c7f7dd745ace17f7e5cce30b385
Viacheslav Sidorov <sid.vp04@gmail.com>
2024-05-05 22:06:54 +0300

    feat: implement simple graph zooming on scroll event

--- src/main/kotlin/view/MainScreen.kt
+++ src/main/kotlin/view/MainScreen.kt
@@ -1,6 +1,7 @@
 package view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.ui.Modifier
@@ -8,15 +9,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.onSizeChanged
 import view.graph.GraphView
 import viewmodel.MainScreenViewModel
+import kotlin.math.exp
+import kotlin.math.sign
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun <V, E> MainScreen(viewModel: MainScreenViewModel<V, E>) {
     var resolution = Pair(0, 0)
     val displayGraph = remember { mutableStateOf(false) }
+
+    var scale by remember { mutableStateOf(1f) }
+    val state = rememberTransformableState { zoomChange, _, _ ->
+        scale *= zoomChange
+    }
+
+    fun scaleBox(delta: Int) {
+        scale = (scale * exp(delta * 0.1f)).coerceIn(0.05f, 4.0f)
+    }
 
     Row(
         horizontalArrangement = Arrangement.spacedBy(20.dp),
@@ -39,8 +55,13 @@ fun <V, E> MainScreen(viewModel: MainScreenViewModel<V, E>) {
                     viewModel.updateOnResize(resolution, Pair(it.width, it.height))
                     resolution = Pair(it.width, it.height)
                 }
+                .onPointerEvent(PointerEventType.Scroll) {
+                    val change = it.changes.first()
+                    val delta = change.scrollDelta.y.toInt().sign
+                    scaleBox(delta)
+                }
         ) {
-            GraphView(viewModel.graphViewModel, displayGraph)
+            GraphView(viewModel.graphViewModel, displayGraph, state, scale)
         }
     }
 }
--- src/main/kotlin/view/graph/GraphView.kt
+++ src/main/kotlin/view/graph/GraphView.kt
@@ -1,22 +1,31 @@
 package view.graph
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import viewmodel.graph.GraphViewModel
 
 @Composable
 fun <V, E> GraphView(
     viewModel: GraphViewModel<V, E>,
-    displayGraph: MutableState<Boolean>
+    displayGraph: MutableState<Boolean>,
+    state: TransformableState,
+    scale: Float
 ) {
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.LightGray)
+        .graphicsLayer(
+            scaleX = scale,
+            scaleY = scale
+        )
+        .transformable(state = state)
     ) {
         if (displayGraph.value) {
             viewModel.edges.forEach { e ->
