d448fc9e662fda92a63f91ac7b05907f8235eb5b
Rudnev Danila <dabzelos@gmail.com>
2024-05-08 04:10:18 +0300

    added types and build architecture

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import model.graph.Graph
-import model.graph.UndirectedGraph
+import graphs.primitives.Graph
+import graphs.types.DirectedGraph
+import graphs.types.UndirectedGraph
+import graphs.types.WeightedDirectedGraph
+import graphs.types.WeightedUndirectedGraph
 import view.MainScreen
 import viewmodel.MainScreenViewModel
 
@@ -19,8 +22,8 @@ val sampleGraph: Graph<String, Long> = UndirectedGraph<String, Long>().apply {
     addVertex("F")
     addVertex("G")
 
-    addEdge("A", "B", 1)
-    addEdge("A", "C", 2)
+    addEdge("A", "B", 1, 1)
+    addEdge("A", "C", 2, 1)
     addEdge("A", "D", 3)
     addEdge("A", "E", 4)
     addEdge("A", "F", 5)
