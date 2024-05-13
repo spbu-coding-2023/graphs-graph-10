42ce3fc722b33bc74f78fa7ca12e4ffa7cfbe613
Sibiri4ok <danilusoltsev05@gmail.com>
2024-05-13 10:29:07 +0300

    fead: add findBridges graph

---
+++ src/main/kotlin/graphs/algo/findBridges.kt
@@ -0,0 +1,38 @@
+package graphs.algo
+import graphs.primitives.Graph
+//!! will be fixed soon
+fun <V> findBridges(graph: Graph<V, *>) : MutableList<Pair<V,V>> {
+    val dictGraph = toAdjacencyList(graph)
+    val visited = mutableSetOf<V>()
+    val low = mutableMapOf<V, Int>()
+    val disc = mutableMapOf<V, Int>()
+    var time = 0
+    val bridges = mutableListOf<Pair<V, V>>()
+
+    fun dfs(u: V, parent: V?) {
+        visited.add(u)
+        disc[u] = time
+        low[u] = time
+        time++
+
+        for ((v, _) in dictGraph[u] ?: emptyList()) {
+            if (v !in visited) {
+                dfs(v, u)
+                low[u] = minOf(low[u]!!, low[v]!!)
+                if (low[v]!! > disc[u]!!) {
+                    bridges.add(v to u)
+                }
+            } else if (v != parent) {
+                low[u] = minOf(low[u]!!, disc[v]!!)
+            }
+        }
+    }
+
+    for (vertex in dictGraph.keys) {
+        if (vertex !in visited) {
+            dfs(vertex, null)
+        }
+    }
+
+    return bridges
+}
