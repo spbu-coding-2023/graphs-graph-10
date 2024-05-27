3f0891a3ef4858fa8e365905704a77605a4572b8
Rudnev Danila <dabzelos@gmail.com>
2024-05-27 08:55:54 +0300

    Kruskal Test

--- src/test/kotlin/graphs/algo/KraskalTest.kt
+++ src/test/kotlin/graphs/algo/KraskalTest.kt
@@ -5,9 +5,10 @@ import graphs.types.WeightedUndirectedGraph
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class KraskalTest {
+class KruskalTest {
+
     @Test
-    fun KraskalTest() {
+    fun SizeOfMST() {
         val graph = WeightedUndirectedGraph().apply() {
             addEdge(0, 1, 1, 7)
             addEdge(0, 2, 2, 8)
@@ -16,20 +17,70 @@ class KraskalTest {
             addEdge(2, 3, 5, 6)
             addEdge(2, 4, 6, 9)
             addEdge(3, 4, 7, 11)
-            addEdge(3, 5, 8, )
-            addEdge(4, 5, 9)
+            addEdge(3, 5, 8, 9)
+            addEdge(4, 5, 9, 10)
             addVertex(0, "A")
             addVertex(1, "B")
             addVertex(2, "C")
             addVertex(3, "D")
-            addVertex(4, "D")
-            addVertex(5, "E")
-            addVertex(6, "F")
-
-
+            addVertex(4, "E")
+            addVertex(5, "F")
         }
-
+        val minimalSpanningtree = Kruskal(graph)
+        assertEquals(minimalSpanningtree.size, 6)
     }
 
+    @Test
+    fun SumOfWeightsInMST() {
+        val graph = WeightedUndirectedGraph().apply() {
+            addEdge(0, 1, 1, 7)
+            addEdge(0, 2, 2, 8)
+            addEdge(1, 2, 3, 11)
+            addEdge(1, 3, 4, 2)
+            addEdge(2, 3, 5, 6)
+            addEdge(2, 4, 6, 9)
+            addEdge(3, 4, 7, 11)
+            addEdge(3, 5, 8, 9)
+            addEdge(4, 5, 9, 10)
+            addVertex(0, "A")
+            addVertex(1, "B")
+            addVertex(2, "C")
+            addVertex(3, "D")
+            addVertex(4, "E")
+            addVertex(5, "F")
+        }
+        val minimalSpanningtree = Kruskal(graph)
+        var sum: Long = 0
+        minimalSpanningtree.forEach { it ->
+            sum += it.weight ?: 0L
+        }
+        assertEquals(33, sum)
+    }
 
+    @Test
+    fun PickedEdgesInMst() {
+        val graph = WeightedUndirectedGraph().apply() {
+            addEdge(0, 1, 1, 7)
+            addEdge(0, 2, 2, 8)
+            addEdge(1, 2, 3, 11)
+            addEdge(1, 3, 4, 2)
+            addEdge(2, 3, 5, 6)
+            addEdge(2, 4, 6, 9)
+            addEdge(3, 4, 7, 11)
+            addEdge(3, 5, 8, 9)
+            addEdge(4, 5, 9, 10)
+            addVertex(0, "A")
+            addVertex(1, "B")
+            addVertex(2, "C")
+            addVertex(3, "D")
+            addVertex(4, "E")
+            addVertex(5, "F")
+        }
+        val minimalSpanningtree = Kruskal(graph)
+        val listOfEdgeIds = mutableListOf<Long?>()
+        minimalSpanningtree.forEach { it ->
+            listOfEdgeIds.add(it.element)
+        }
+        assertEquals(listOfEdgeIds, listOf<Long>(4, 5, 1, 6, 8))
+    }
 }
