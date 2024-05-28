389af16d9b164087b0b0df75aefc51eacb8f357f
Avgust Varlamov <varl.avgust@yandex.ru>
2024-05-28 19:30:59 +0300

    fix: choose optimize algorithm for search SCC

--- src/main/kotlin/graphs/algo/findSCC.kt
+++ src/main/kotlin/graphs/algo/findSCC.kt
@@ -5,11 +5,11 @@ import graphs.primitives.Graph
 import graphs.primitives.Vertex
 
 
-fun <V, E> findSCC(graph: Graph<V, E>): List<List<Vertex<V>>> {
-    val stack = mutableListOf<Vertex<V>>()
-    val visited = mutableSetOf<Vertex<V>>()
+fun findSCC(graph: Graph): List<List<Vertex>> {
+    val stack = mutableListOf<Vertex>()
+    val visited = mutableSetOf<Vertex>()
 
-    fun dfs1(vertex: Vertex<V>) {
+    fun dfs1(vertex: Vertex) {
         visited.add(vertex)
         for (edge in graph.edges) {
             if (edge.vertices.first == vertex && edge.vertices.second !in visited) {
@@ -20,42 +20,42 @@ fun <V, E> findSCC(graph: Graph<V, E>): List<List<Vertex<V>>> {
     }
 
     for (vertex in graph.vertices) {
-        if (vertex !in visited)
+        if (vertex !in visited) {
             dfs1(vertex)
+        }
     }
 
-//    val reverseGraph = getReverseGraph(graph)
-//    visited.clear()
-//
-//    fun dfs2(vertex: Vertex<V>, component: MutableList<Vertex<V>>) {
-//        visited.add(vertex)
-//        component.add(vertex)
-//        for (edge in reverseGraph.edges) {
-//            if (edge.vertices.second == vertex && edge.vertices.first !in visited) {
-//                dfs2(edge.vertices.first, component)
-//            }
-//        }
-//    }
-//
-    val stronglyConnectedComponents = mutableListOf<List<Vertex<V>>>()
-//
-//    while (stack.isNotEmpty()) {
-//        val vertex = stack.removeAt(stack.size - 1)
-//        if (vertex !in visited) {
-//            val component: MutableList<Vertex<V>> = mutableListOf()
-//            dfs2(vertex, component)
-//            stronglyConnectedComponents.add(component)
-//        }
-//    }
+    val reversedEdges = createReversedEdges(graph)
 
-    return stronglyConnectedComponents
+    visited.clear()
+    val components = mutableListOf<List<Vertex>>()
+
+    fun dfs2(vertex: Vertex, component: MutableList<Vertex>) {
+        visited.add(vertex)
+        component.add(vertex)
+        for (edge in reversedEdges) {
+            if (edge.first == vertex && edge.second !in visited) {
+                dfs2(edge.second, component)
+            }
+        }
+    }
+
+    while (stack.isNotEmpty()) {
+        val vertex = stack.removeAt(stack.size - 1)
+        if (vertex !in visited) {
+            val component = mutableListOf<Vertex>()
+            dfs2(vertex, component)
+            components.add(component)
+        }
+    }
+
+    return components
 }
 
-//fun <V, E> getReverseGraph(graph: Graph<V, E>): Graph<V, E> {
-//    val reverseGraph = Graph<V, E>()
-//    reverseGraph.vertices.addAll(graph.vertices)
-//    for (edge in graph.edges) {
-//        reverseGraph.edges.add(Edge(edge.element, edge.to, edge.from))
-//    }
-//    return reverseGraph
+private fun createReversedEdges(graph: Graph): List<Pair<Vertex, Vertex>> {
+    val res = mutableListOf<Pair<Vertex, Vertex>>()
+    for (e in graph.edges) {
+        res.add(Pair(e.vertices.second, e.vertices.first))
+    }
+    return res
 }
