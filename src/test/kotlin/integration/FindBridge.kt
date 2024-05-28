b25829e5c84a9099d411ba091ff65ce4a2facad4
Rudnev Danila <dabzelos@gmail.com>
2024-05-28 18:01:52 +0300

    new test fo Dijkstra

--- src/test/kotlin/integration/FindBridge.kt
+++ src/test/kotlin/integration/FindBridge.kt
@@ -3,33 +3,31 @@ package integration
 import androidx.compose.ui.graphics.Color
 import graphs.algo.findBridges
 import graphs.types.UndirectedGraph
-import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
 import view.algo.drawFindBridge
 import viewmodel.MainScreenViewModel
 import kotlin.test.assertEquals
 
-class FindBridgeIntegrationTest {
+class FindBridgeIntegrationTest() {
     private val sampleGraph = UndirectedGraph().apply {
         addVertex(0)
         addVertex(1)
         addVertex(2)
         addVertex(3)
+        addVertex(4)
         addEdge(1, 0, 0)
         addEdge(2, 1, 1)
         addEdge(2, 0, 2)
         addEdge(0, 3, 3)
         addEdge(3, 4, 4)
     }
-
-    private val mainViewModel = MainScreenViewModel(sampleGraph)
-
+    val mainViewModel = MainScreenViewModel(sampleGraph)
     @Test
-    fun FindBridgeIntegrationTest() = runBlocking {
+    fun FindBridgeIntegrationTest() {
+
         mainViewModel.runLayoutAlgorithm(Pair(800, 600))
 
-        drawFindBridge(mainViewModel.graphViewModel).join()
-
+        drawFindBridge(mainViewModel.graphViewModel)
         val bridges = findBridges(sampleGraph)
 
         var countBridges = 0
@@ -44,4 +42,6 @@ class FindBridgeIntegrationTest {
         }
         assertEquals(countBridges, bridges.size, "The count of extended edges does not match the count of real bridges")
     }
+
+
 }
