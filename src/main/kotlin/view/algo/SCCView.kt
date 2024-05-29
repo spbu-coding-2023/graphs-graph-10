81496685085257c31a3432452dcd279da97afdaf
Rudnev Danila <dabzelos@gmail.com>
2024-05-29 14:18:31 +0300

    bugfix scc

--- src/main/kotlin/view/algo/SCCView.kt
+++ src/main/kotlin/view/algo/SCCView.kt
@@ -7,12 +7,11 @@ fun drawSCC(graphViewModel: GraphViewModel) {
     val communities = findSCC(graphViewModel.graph)
     val colors = generateRandomColors(communities.size)
     for (i in communities.indices) {
-        if (communities[i].size > 1) {
-            for (j in communities[i].indices) {
-                graphViewModel.vertices.forEach { v ->
-                    if (v.v.element == communities[i][j].element)
-                        v.color = colors[i]
-                }
+        println(communities[i])
+        for (j in communities[i].indices) {
+            graphViewModel.vertices.forEach { v ->
+                if (v.v.element == communities[i][j].element)
+                    v.color = colors[i]
             }
         }
     }
