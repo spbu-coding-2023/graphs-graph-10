920c0b51ae4d2d5aa89fd69485b0c128b4355013
Viacheslav Sidorov <sid.vp04@gmail.com>
2024-07-29 19:18:45 +0300

    feat: add gradle target to bundle application to jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,15 +29,34 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 }
 
+
 compose.desktop {
+
     application {
         mainClass = "MainKt"
-
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+
+            targetFormats(TargetFormat.Dmg, TargetFormat.Exe, TargetFormat.Deb)
             packageName = "graphs"
             packageVersion = "1.0.0"
         }
+    }
+}
+tasks.register<Zip>("repackageReleaseUberJar") {
+    val packageUberJarForCurrentOS = tasks.getByName("packageReleaseUberJarForCurrentOS")
+    dependsOn(packageUberJarForCurrentOS)
+    val file = packageUberJarForCurrentOS.outputs.files.first()
+    val output = File(file.parentFile, "${file.nameWithoutExtension}-repacked.jar")
+    archiveFileName.set(output.absolutePath)
+    destinationDirectory.set(file.parentFile.absoluteFile)
+    exclude("META-INF/*.SF")
+    exclude("META-INF/*.RSA")
+    exclude("META-INF/*.DSA")
+    from(project.zipTree(file))
+    doLast {
+        delete(file)
+        output.renameTo(file)
+        logger.lifecycle("The repackaged jar is written to ${archiveFile.get().asFile.canonicalPath}")
     }
 }
 
